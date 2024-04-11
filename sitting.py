9fc772435549b9082976f9faddbdc75653194ca0
Priscilla <priscillamikisa@gmail.com>
2024-04-11 19:55:32 +0300

    added a file

---
+++ sitting.py
@@ -0,0 +1,7 @@
+def sittingArragement(name,mark):
+    if (mark >= 80):
+        print(f"{name} in AnitaB Lab")
+    if (mark < 80 & mark>=60):
+        print(f"{name} in Lovelace Lab")
+    else:
+        print(f"{name} in Ada Lab") 
